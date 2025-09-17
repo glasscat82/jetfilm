6d00fc00d4d5c54394c075c94deecf47ddd1be26
glasscat82 <glasscat82@gmail.com>
2025-09-17 12:57:41 +0200

    🌵

--- main.go
+++ main.go
@@ -5,20 +5,89 @@ import (
 	"fmt"
 	"log"
 	"net/http"
+	"strconv"
+	"strings"
 
 	"github.com/PuerkitoBio/goquery"
 )
 
 type ItemParse struct {
-	Title   string `json:"title"`
-	Href    string `json:"href"`
-	Genres  string `json:"genres"`
-	Img     string `json:"img"`
-	Types   string `json:"types"`
-	Episode string `json:"episode"`
+	Title   string  `json:"title"`
+	Href    string  `json:"href"`
+	Genres  string  `json:"genres"`
+	Img     string  `json:"img"`
+	Types   string  `json:"types"`
+	Episode Episode `json:"episode"`
+	Rating  Rating  `json:"rating"`
+}
+
+type Rating struct {
+	Kp   float64 `json:"kp"`
+	Imdb float64 `json:"imdb"`
+	Jet  float64 `json:"jet"`
+}
+
+type Episode struct {
+	Season int `json:"season"`
+	Series int `json:"series"`
 }
 
 var Items []ItemParse
+
+func SetFloat(s string) float64 {
+	if s == "" {
+		return 0
+	}
+
+	s = strings.TrimSpace(s)
+	num, err := strconv.ParseFloat(s, 64)
+	if err != nil {
+		return 0
+	}
+
+	return num
+}
+
+func GetRating(film *goquery.Selection) Rating {
+	rating := film.Find(".film-item-rating")
+	kp := rating.Find(".kp").Text()
+	imdb := rating.Find(".imdb").Text()
+	jet := rating.Find(".jet").Text()
+
+	return Rating{Kp: SetFloat(kp), Imdb: SetFloat(imdb), Jet: SetFloat(jet)}
+}
+
+func GetEpisode(film *goquery.Selection) (Episode, error) {
+	episode := film.Find(".film-item-episode").Text()
+	//1 сезон 10 серия
+	epi := strings.TrimSpace(episode)
+	if epi == "" {
+		return Episode{}, errors.New("not episode")
+	}
+
+	var Season int
+	var Series int
+	episodeArr := strings.Split(epi, " ")
+	if len(episodeArr[0]) != 0 {
+		season, err := strconv.Atoi(episodeArr[0])
+		if err != nil {
+			return Episode{}, errors.New("not season")
+		}
+
+		Season = season
+	}
+
+	if len(episodeArr[2]) != 0 {
+		series, err := strconv.Atoi(episodeArr[2])
+		if err != nil {
+			return Episode{}, errors.New("not series")
+		}
+
+		Series = series
+	}
+
+	return Episode{Season: Season, Series: Series}, nil
+}
 
 func GetItem(i int, s *goquery.Selection) {
 	// For each item found, get the title
@@ -30,17 +99,19 @@ func GetItem(i int, s *goquery.Selection) {
 	film := s.Find(".film-item-image")
 	img, _ := film.Find("img").Attr("data-src")
 	types := film.Find(".film-item-type").Text()
-	episode := film.Find(".film-item-episode").Text()
-
-	// rating := film.Find(".film-item-rating")
 
 	item := ItemParse{
-		Title:   title,
-		Href:    href,
-		Genres:  genres,
-		Img:     img,
-		Types:   types,
-		Episode: episode,
+		Title:  title,
+		Href:   href,
+		Genres: genres,
+		Img:    img,
+		Types:  strings.TrimSpace(types),
+		Rating: GetRating(film),
+	}
+
+	episode, err := GetEpisode(film)
+	if err == nil {
+		item.Episode = episode
 	}
 
 	Items = append(Items, item)
